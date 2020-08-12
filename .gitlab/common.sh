468166865a7a00c9f4fbb016d39ad822ae979f08
Pipeline Service <pipeline-service-sce@salt-software.de>
2020-08-12 15:22:17 +0000

    Release pipeline-S24

--- .gitlab/common.sh
+++ .gitlab/common.sh
@@ -101,7 +101,7 @@ function prepare-fossa() {
 function foss-analysis() {
   prepare-fossa
   printf "Perform Fossa Analysis\n"
-  fossa analyze --verbose -T "Logistik"
+  fossa analyze --verbose -T "SSG Logistik"
 }
 
 function prepare-sonarqube() {
