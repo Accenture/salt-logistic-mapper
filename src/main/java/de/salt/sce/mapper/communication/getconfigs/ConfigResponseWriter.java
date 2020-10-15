fc736ce8140a1123768b635b0c4111da94bed5c6
Zohrabyan, Davit <davit.zohrabyan@salt-software.de>
2020-10-15 14:56:49 +0200

    HOTFIX: smooks config environment variable on logs

--- src/main/java/de/salt/sce/mapper/communication/getconfigs/ConfigResponseWriter.java
+++ src/main/java/de/salt/sce/mapper/communication/getconfigs/ConfigResponseWriter.java
@@ -3,17 +3,21 @@ package de.salt.sce.mapper.communication.getconfigs;
 
 import de.salt.sce.mapper.communication.getconfigs.model.ConfigResponse;
 import de.salt.sce.mapper.communication.getconfigs.model.SmooksConfigFile;
+import org.slf4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
 
+import static java.lang.String.format;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.apache.commons.io.FileUtils.writeStringToFile;
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Writing smooks configuration files in given path.
  */
 public class ConfigResponseWriter {
+    private static final Logger log = getLogger(ConfigResponseWriter.class);
 
     private static final String PLACEHOLDER = "PATH_TO_FOLDER";
 
@@ -22,14 +26,17 @@ public class ConfigResponseWriter {
      * Replacing in config files PLACEHOLDER to Absolute path to file.
      * This needs for smooks. Sometimes smooks configs contain 2 or more files, with links( or include) another files.
      *
-     * @param rootFolder                  Path in local filesystem
+     * @param rootFolder     Path in local filesystem
      * @param configResponse {@link ConfigResponse} content to store.
      */
     public static void replaceAndWrite(String rootFolder, ConfigResponse configResponse) throws IOException {
         String path = createFilePath(rootFolder, configResponse);
+        log.trace(format("Create Smooks Configs On Path: %s", path));
         for (SmooksConfigFile smooksFile : configResponse.getFiles()) {
+            String filePath = format("%s/%s", path, smooksFile.getFileName());
+            log.trace(format("Create Smooks Config File: %s", filePath));
             writeStringToFile(
-                    new File(path + "/" + smooksFile.getFileName()),
+                    new File(filePath),
                     smooksFile.getFileContent().replaceFirst(PLACEHOLDER, path),
                     UTF_8
             );
