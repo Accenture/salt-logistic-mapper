2fe31ce7a66edddb66411b28b7df201ec185b6fc
dzh <davit.zohrabyan@salt-solutions.de>
2021-02-25 10:36:48 +0100

    test

--- .gitlab/S-PUBLISH-github.sh
+++ .gitlab/S-PUBLISH-github.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-printf "Hallo GitHub\n"
+source .gitlab/github-vars.sh
+
+project_name="$(echo "$project" | awk -F "/" '{print $NF}' | awk -F "." '{print $1}')"
+printf "Project name: %s\n" "$project_name"
+printf "GIT repo: %s\n" "$project"
+
+printf "Clone git repository...\n"
+gitlab_address="https://$gitlab_user:$gitlab_token@$(echo "$project" | sed -e 's#https://##')"
+git clone "$gitlab_address"
+
+printf "Change directory to $project_name...\n"
+cd "$project_name" || exit
+
+printf "Delete pipeline tags - if exists...\n"
+for tag in $(git tag | grep pipeline); do
+  git tag -d "$tag"
+  git push --delete origin "$tag"
+done
+
+printf "Delete gitlab-ci file and gitlab folder...\n"
+git rm .gitlab-ci.yml
+git rm -rf .gitlab
+
+github_address="https://$github_user:$github_password@$(echo "$github_url" | sed -e 's#https://##')"
+printf "Copy into $github_address\n"
+
+git config --global user.email "$github_user_email"
+git config --global user.name "$github_user"
+git remote add github "$github_address"
+git commit -m "$github_commit_message"
+git push github master --tags -f
+
+cd ..
+printf "\n"
+printf "==============================================================================================================\n"
+printf "Done, The $project_name project has successfully into GitHub($github_url) copied! \n"
+printf "==============================================================================================================\n"
---
+++ .gitlab/github-vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Variables
+export project="https://swugit1.salt-solutions.de/sce/common/github_test.git"
+
+export gitlab_user=pipeline-service-sce
+export gitlab_user_email=pipeline-service-sce@salt-software.de
+export gitlab_user_name="Pipeline Service"
+
+export github_user_email="Davit.Zohrabyan@salt-solutions.de"
+export github_user="salt-logistic-dzh"
+export github_password="aN59KccRSV"
+export github_url="https://github.com/Accenture/salt-logistic-mapper.git"
+export github_commit_message="Publish in GitHub."
